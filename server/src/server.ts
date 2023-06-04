5c93574be7f085f1b31b63aa491248d1ba6d3ff2
Carlos Eduardo <cadu4duarte@gmail.com>
2023-06-03 21:35:38 -0300

    feat: create web project

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,16 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.get('/hello', () => {
-	return "hello world";
+app.get('/users', async () => {
+	const users = await prisma.user.findMany()
+	return users;
 });
 
 app.listen({
-	port: 3333,
+		port: 3333,
 }).then(() => {
 	console.log("server running on http://localhost:3333");
 });
