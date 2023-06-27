da2b70dbc53b27f7dd8bf7795620588df481c66f
Carlos Eduardo <cadu4duarte@gmail.com>
2023-06-26 21:44:07 -0300

    feat: edit layout

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,19 @@
 
 export default function Home() {
-  return(
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+  return (
+    <main className="grid grid-cols-2 min-h-screen">
+      <div className="flex flex-col items-start justify-between px-28 py-28">
+
+      </div>
+
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, começe a {''}
+            <a href="" className="underline hover:text-gray-50">criar agora!</a>
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
