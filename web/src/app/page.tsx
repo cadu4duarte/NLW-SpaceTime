8faf4d07be8684c382b68469a8a790bf11771a3f
Carlos Eduardo <cadu4duarte@gmail.com>
2023-06-27 21:28:17 -0300

    feat: change layout

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,10 +2,17 @@
 export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
-      <div className="flex flex-col items-start justify-between px-28 py-28">
+      {/*left*/}
+      <div className="flex flex-col items-start justify-between px-28 py-28 relative overflow-hidden border-r border-white/10">
 
+        {/*blur*/}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"/>
+
+        {/*stripes*/}
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes"/>
       </div>
-
+     
+      {/* right */}
       <div className="flex flex-col p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="text-center leading-relaxed w-[360px]">
