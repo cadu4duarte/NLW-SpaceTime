3405a6b00c075da7d283d4c6e3d88e0bd5e6331d
Carlos Eduardo <cadu4duarte@gmail.com>
2023-07-27 21:59:48 -0300

    feat: change code

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
       <div className="flex flex-col items-start justify-between px-28 py-28 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
 
         {/*blur*/}
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"/>
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
         {/*stripes*/}
-        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes"/>
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
 
         <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ">
@@ -20,13 +20,13 @@ export default function Home() {
           </div>
 
           <p className="text-sm leading-snug max-w-[140px]">
-              <span className="underline">Crie sua conta</span> e salve suas memórias!
-            </p>
+            <span className="underline">Crie sua conta</span> e salve suas memórias!
+          </p>
         </a>
 
         {/*hero*/}
         <div className="space-y-5">
-          <Image src={nlwLogo} alt="nlw Space Time"/>
+          <Image src={nlwLogo} alt="nlw Space Time" />
 
           <div className="max-w-[420px] space-y-1">
             <h1 className="text-5xl font-bold leading-tight text-gray-50">Sua cápsula do tempo</h1>
@@ -39,12 +39,13 @@ export default function Home() {
 
         </div>
 
-        <div>
+        {/*copyright*/}
+        <div className="text-sm leading-relaxed text-gray-200">
           Desenvolvido por Carlos Eduardo
         </div>
 
       </div>
-     
+
       {/* right */}
       <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div className="flex flex-1 items-center justify-center">
