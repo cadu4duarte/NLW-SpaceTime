1e7eba7009737035ea421c862f2bdf1d43034335
Carlos Eduardo <cadu4duarte@gmail.com>
2023-08-14 20:51:56 -0300

    feat: create new routes

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -33,17 +33,69 @@ export async function memoriesRoutes(app: FastifyInstance) {
         id,
       },
     })
+
+    return memory
 	});
 
-	app.post('/memories', async () => {
-		
+	app.post('/memories', async (request) => {
+		const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false)
+    })
+
+    const {content, coverUrl, isPublic} = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: '311b66b4-e056-40b8-b3a5-da68234cc6cb',
+      },
+    })
+
+    return memory
 	});
 
-	app.put('/memories/:id', async () => {
-		
+	app.put('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const {id} = paramsSchema.parse(request.params)
+
+		const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false)
+    })
+
+    const {content, coverUrl, isPublic} = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
 	});
 
-	app.delete('/memories/:id', async () => {
-		
+	app.delete('/memories/:id', async (request) => {
+		const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const {id} = paramsSchema.parse(request.params)
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      }
+    })
 	});
 }
